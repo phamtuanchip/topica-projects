8c451f939f6275647a6d29e7fb7664d634420a15
phamtuanchip <phamtuanchip@d272c6e3-baf3-dffc-3d30-7ea27df30e5f>
2011-04-05 09:54:32 +0000

    startup practice 1

---
+++ IT202.G1-G2/src/p1/Tuanpham.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2003-2011 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package p1;
+
+import javax.swing.JFrame;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ *          exo@exoplatform.com
+ * Apr 5, 2011  
+ */
+public class Tuanpham {
+
+  public Tuanpham(){
+    
+    
+  }
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+    System.out.println("\n\n hello !");
+    JFrame j = new JFrame();
+    j.setBounds(100, 100, 300, 300);
+    j.show();
+    
+    
+    
+  }
+
+}
